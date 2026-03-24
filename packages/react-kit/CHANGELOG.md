04fd5160b76b3fc8415fee0aa0efafa7a8097cc1
Omar Basem <omarbasem.0101@gmail.com>
2026-03-24 08:20:42 +0400

    feat: create react-kit package

---
+++ packages/react-kit/CHANGELOG.md
@@ -0,0 +1,8 @@
+# @zerodev/wallet-react-kit
+
+## 0.0.1
+
+### Patch Changes
+
+- Initial release
+
---
+++ packages/react-kit/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @zerodev/wallet-react-kit
+ * React UI components for ZeroDev Wallet SDK
+ */
+
+// Export components here as you build them
+// Example:
+// export { Button } from './components/Button'
+// export { LoginModal } from './components/LoginModal'
+
+export const version = '0.0.1'
