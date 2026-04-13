48aa240c25699707b5310f317be6607f52564467
Omar Basem <omarbasem.0101@gmail.com>
2026-04-13 11:43:49 +0400

    feat: ScreenWrapper component

---
+++ packages/react-kit/src/shared/components/ScreenWrapper/index.tsx
@@ -0,0 +1,28 @@
+import { MultiRadialBackground } from './MultiRadialBackground'
+
+interface ChildrenProps {
+  paddingTop: number
+  paddingBottom: number
+}
+
+const TOP_NAV_HEIGHT = 52
+const BOTTOM_TAB_HEIGHT = 56
+const BOTTOM_TAB_MARGIN = 20
+
+export function ScreenWrapper({
+  children,
+}: {
+  children: (props: ChildrenProps) => React.ReactNode
+}) {
+  const paddingBottom = BOTTOM_TAB_HEIGHT + BOTTOM_TAB_MARGIN + 10
+  const paddingTop = TOP_NAV_HEIGHT + 20
+
+  return (
+    <div className="flex-1 flex flex-col">
+      <MultiRadialBackground />
+      <div className="flex-1 bg-[#F7F5F0CC] m-1.5 px-4 overflow-hidden rounded-[42px]">
+        {children({ paddingTop, paddingBottom })}
+      </div>
+    </div>
+  )
+}
