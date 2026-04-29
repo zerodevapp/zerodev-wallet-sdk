1e88b2527dea01be39a274ae93e29008141148c4
Omar Basem <omarbasem.0101@gmail.com>
2026-04-29 11:55:37 +0400

    review

--- packages/react-kit/src/signing/pages/CollectionApproval.tsx
+++ packages/react-kit/src/signing/pages/CollectionApproval.tsx
@@ -7,7 +7,18 @@ import { ArrowCardPair } from '../components/ArrowCardPair'
 import { InfoCard } from '../components/InfoCard'
 import { SigningLayout } from '../components/SigningLayout'
 import { type GasFee, type GasTier, TxGasFees } from '../components/TxGasFees'
-import { type Dapp, TxInformation } from '../components/TxInformation'
+
+const COLLECTION_IMAGE_SOURCE =
+  'https://api.dicebear.com/7.x/shapes/svg?seed=collection'
+const SPENDER_IMAGE_SOURCE =
+  'https://api.dicebear.com/7.x/identicon/svg?seed=spender'
+const SELECTED_GAS_TIER: GasTier = 'market'
+const GAS_FEES: GasFee[] = [
+  { tier: 'low', duration: 60, fee: '0.0002 ETH', feeUsd: '$0.50' },
+  { tier: 'market', duration: 30, fee: '0.0004 ETH', feeUsd: '$1.00' },
+  { tier: 'fast', duration: 15, fee: '0.0008 ETH', feeUsd: '$2.00' },
+]
+const SLIPPAGE = 0.5
 
 const nameAbi = [
   {
@@ -25,12 +36,6 @@ interface CollectionApprovalProps {
   approved: boolean
   confirm: () => void
   reject: () => void
-  dapp: Dapp
-  selectedGasTier: GasTier
-  gasFees: GasFee[]
-  slippage?: number
-  collectionImageSource: string
-  spenderImageSource: string
 }
 
 export function CollectionApproval({
@@ -39,12 +44,6 @@ export function CollectionApproval({
   approved,
   confirm,
   reject,
-  dapp,
-  selectedGasTier,
-  gasFees,
-  slippage,
-  collectionImageSource,
-  spenderImageSource,
 }: CollectionApprovalProps) {
   const { data: name, isLoading } = useReadContract({
     address: contract,
@@ -76,7 +75,6 @@ export function CollectionApproval({
               : `Revoke this contract's permission to manage your ${collectionName} tokens.`}
           </Text>
         </div>
-        <TxInformation dapp={dapp} />
         <div className="flex flex-col gap-2">
           <Text className="text-body1 pt-2 px-2">
             You&#39;re approving permission to
@@ -86,21 +84,21 @@ export function CollectionApproval({
               <InfoCard
                 title={collectionName}
                 subtitle={cardSubtitle}
-                imageSource={collectionImageSource}
+                imageSource={COLLECTION_IMAGE_SOURCE}
               />
             }
             bottomCard={
               <InfoCard
                 title="Spender"
                 subtitle={shortenHex(operator)}
-                imageSource={spenderImageSource}
+                imageSource={SPENDER_IMAGE_SOURCE}
               />
             }
           />
           <TxGasFees
-            selectedGasTier={selectedGasTier}
-            gasFees={gasFees}
-            {...(slippage !== undefined && { slippage })}
+            selectedGasTier={SELECTED_GAS_TIER}
+            gasFees={GAS_FEES}
+            slippage={SLIPPAGE}
           />
         </div>
       </div>
