0193517fb89d63e7d9d101d2c32237b59d89aa33
Omar Basem <omarbasem.0101@gmail.com>
2026-05-04 16:42:49 +0400

    feat(react-kit): SignatureRequest custom classname (#147)

--- packages/react-kit/src/shared/components/ScreenWrapper/index.tsx
+++ packages/react-kit/src/shared/components/ScreenWrapper/index.tsx
@@ -1,3 +1,5 @@
+import type { CSSProperties } from 'react'
+import { cn } from '../../utils/common'
 import { MultiRadialBackground } from './MultiRadialBackground'
 
 interface ChildrenProps {
@@ -11,14 +13,24 @@ const BOTTOM_TAB_MARGIN = 20
 
 export function ScreenWrapper({
   children,
+  className,
+  style,
 }: {
   children: (props: ChildrenProps) => React.ReactNode
+  className?: string | undefined
+  style?: CSSProperties | undefined
 }) {
   const paddingBottom = BOTTOM_TAB_HEIGHT + BOTTOM_TAB_MARGIN + 10
   const paddingTop = TOP_NAV_HEIGHT + 20
 
   return (
-    <div className="flex-1 flex flex-col relative overflow-hidden h-full">
+    <div
+      className={cn(
+        'flex-1 flex flex-col relative overflow-hidden h-full',
+        className,
+      )}
+      style={style}
+    >
       <MultiRadialBackground />
       <div className="flex-1 bg-offWhite/85 m-1.5 px-4 overflow-hidden rounded-[30px] relative">
         {children({ paddingTop, paddingBottom })}
