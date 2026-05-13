2f417ccdf5a9b455d078df50ad73321bc7823b56
Andras Szucs <andras@andras.codes>
2026-05-13 18:58:17 +0200

    feat: add expo-example app (#76)

---
+++ apps/expo-example/wagmi.config.ts
@@ -0,0 +1,36 @@
+import { zeroDevWallet } from '@zerodev/wallet-react'
+import { createConfig, createStorage, http } from 'wagmi'
+import { arbitrumSepolia, sepolia } from 'wagmi/chains'
+import { asyncSessionStorage } from './lib/asyncSessionStorage'
+import { createReactNativePasskeyStamper } from './lib/reactNativePasskeyStamper'
+import { createSecureStoreStamper } from './lib/secureStoreStamper'
+
+const ZERODEV_PROJECT_ID = process.env.EXPO_PUBLIC_ZERODEV_PROJECT_ID ?? ''
+const RP_ID = 'com.yourapp.bundleid'
+const chains = [sepolia, arbitrumSepolia] as const
+
+export const wagmiConfig = createConfig({
+  chains,
+  connectors: [
+    zeroDevWallet({
+      projectId: ZERODEV_PROJECT_ID,
+      chains,
+      rpId: RP_ID,
+      fetchOptions: {
+        headers: { Origin: 'http://localhost:8081' },
+      },
+      apiKeyStamper: createSecureStoreStamper(),
+      passkeyStamper: createReactNativePasskeyStamper({
+        rpId: RP_ID,
+      }),
+      sessionStorage: asyncSessionStorage,
+      persistStorage: asyncSessionStorage,
+    }),
+  ],
+  transports: {
+    [sepolia.id]: http(),
+    [arbitrumSepolia.id]: http(),
+  },
+  storage: createStorage({ storage: asyncSessionStorage }),
+  multiInjectedProviderDiscovery: false,
+})
