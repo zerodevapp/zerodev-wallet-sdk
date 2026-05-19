f3addf6b81d65ea00b93332771f197029ece0d7e
Omar Basem <omarbasem.0101@gmail.com>
2026-05-19 18:47:36 +0400

    fix(react-kit): `Input` border radius (#211)

--- packages/react-kit/src/shared/components/Input/index.tsx
+++ packages/react-kit/src/shared/components/Input/index.tsx
@@ -16,6 +16,7 @@ export interface InputProps
   multiline?: boolean
   iconName?: IconName
   children?: React.ReactNode
+  containerClassName?: string
 }
 
 function getHeightClass(multiline: boolean | undefined, variant: Variant) {
@@ -50,6 +51,7 @@ export function Input({
   iconName,
   children,
   className,
+  containerClassName,
   onFocus,
   onBlur,
   ...inputProps
@@ -119,6 +121,7 @@ export function Input({
         'flex w-full rounded-2xl items-center gap-3 pr-2',
         heightClass,
         paddingLeft,
+        containerClassName,
       )}
       variant={isFocused ? 'solid' : 'soft'}
     >
