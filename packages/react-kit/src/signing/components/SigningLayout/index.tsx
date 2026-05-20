84b2015e1457059818c5e8653f6ac09996248863
Omar Basem <omarbasem.0101@gmail.com>
2026-05-20 16:49:10 +0400

    fix mobile signature request

--- packages/react-kit/src/signing/components/SigningLayout/index.tsx
+++ packages/react-kit/src/signing/components/SigningLayout/index.tsx
@@ -20,7 +20,7 @@ export function SigningLayout({
   error,
 }: SigningLayoutProps) {
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex-1 flex flex-col min-h-0">
       <div className="flex-1 min-h-0 overflow-y-auto flex flex-col gap-2 pb-2">
         {children}
         {!!error && <AlertView {...getTxErrorInfo(error)} />}
